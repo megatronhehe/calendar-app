7a528251ed42cd27014f281c42f3f21ae614cc3f
Megatronhehee <satyamahendra09@gmail.com>
2023-08-13 10:23:52 +0700

    feature added: confirmation to delete all task on a selected date

--- src/pages/Main/ActivitiesList.jsx
+++ src/pages/Main/ActivitiesList.jsx
@@ -1,17 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Section from "../../components/Section";
 import ActivityItem from "./ActivityItem";
 import { isSameDay } from "date-fns";
 
+import {
+	BsThreeDotsVertical,
+	BsCheck,
+	BsX,
+	BsFillTrash3Fill,
+} from "react-icons/bs";
+
 const ActivitiesList = ({
 	setActivities,
 	filterActivitiesByDateArray,
 	selectedDate,
-	activities,
 }) => {
+	// States
 	const [toggleMore, setToggleMore] = useState(false);
+	const [toggleDelete, setToggleDelete] = useState(false);
 
+	// reset the confirmation delete button when options is opened
+	useEffect(() => {
+		setToggleDelete(false);
+	}, [toggleMore]);
+
+	// close the option when selected date is changed
+	useEffect(() => {
+		if (toggleMore) {
+			setToggleMore(false);
+		}
+	}, [selectedDate]);
+
+	// functions
 	const setAll = (check) => {
 		setActivities((prev) =>
 			prev.map((activity) =>
@@ -28,6 +49,7 @@ const ActivitiesList = ({
 		);
 	};
 
+	// elements
 	const activitiesElement =
 		filterActivitiesByDateArray &&
 		filterActivitiesByDateArray.map((activity) => (
@@ -41,13 +63,64 @@ const ActivitiesList = ({
 	return (
 		<Section>
 			<div className="h-full pr-2 ">
-				<div className="flex items-center justify-between w-full mb-4 ">
+				<div className="relative flex items-center justify-between w-full mb-4 ">
 					<h2 className="">Activities</h2>
-					<div className="flex gap-6 text-xs">
-						<button onClick={() => setAll(true)}>check all</button>
-						<button onClick={() => setAll(false)}>uncheck all</button>
-						<button onClick={deleteAll}>delete all</button>
-					</div>
+					<button
+						className={`flex items-center justify-center w-6 h-6 rounded-full hover:bg-gray-100 ${
+							toggleMore ? "bg-gray-200" : ""
+						}`}
+						onClick={() => setToggleMore((prev) => !prev)}
+					>
+						<BsThreeDotsVertical />
+					</button>
+					{toggleMore && (
+						<div className="absolute flex flex-col w-32 gap-1 p-1 text-xs text-gray-400 bg-gray-100 shadow-md rounded-xl right-4 top-4">
+							<button
+								onClick={() => setAll(true)}
+								className="flex items-center justify-between px-3 py-2 bg-white x-6 rounded-xl"
+							>
+								check all
+								<BsCheck className="text-base" />
+							</button>
+							<button
+								onClick={() => setAll(false)}
+								className="flex items-center justify-between px-3 py-2 bg-white x-6 rounded-xl"
+							>
+								uncheck all
+								<BsX className="text-base" />
+							</button>
+							<div className="my-1 border-b"></div>
+							<div>
+								{!toggleDelete ? (
+									<button
+										onClick={() => setToggleDelete(true)}
+										className="flex items-center justify-between w-full px-3 py-2 bg-white rounded-xl"
+									>
+										delete all
+										<BsFillTrash3Fill className="text-base" />
+									</button>
+								) : (
+									<div className="flex items-center w-full gap-2 px-3 py-2 bg-white rounded-xl">
+										<button
+											onClick={() => setToggleDelete(false)}
+											className="flex justify-center w-1/2 text-base"
+										>
+											<BsX />
+										</button>
+										<button
+											onClick={() => {
+												deleteAll();
+												setToggleMore(false);
+											}}
+											className="flex justify-center w-1/2 text-base"
+										>
+											<BsCheck />
+										</button>
+									</div>
+								)}
+							</div>
+						</div>
+					)}
 				</div>
 
 				{filterActivitiesByDateArray.length > 0 ? (
